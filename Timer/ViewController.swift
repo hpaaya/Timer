e277a21cc264564eff6dfd66456369a267d577b1
User10 <user10@user10de-imac.local>
2018-12-03 16:58:59 +0800

    Initial Commit

---
+++ Timer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Timer
+//
+//  Created by User10 on 2018/12/3.
+//  Copyright © 2018 kelly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
